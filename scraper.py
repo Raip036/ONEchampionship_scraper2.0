45d224c91e79059b68ebbc5af81e68431bb07681
Pratik <pratikrai@hotmail.co.uk>
2025-09-06 21:04:49 +0100

    Working scraper for fighters on 1st page

--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import csv
+import time
 
 # Set up ChromeDriver service
 service = Service('/opt/homebrew/bin/chromedriver')
@@ -9,20 +11,48 @@ service = Service('/opt/homebrew/bin/chromedriver')
 driver = webdriver.Chrome(service=service)
 
 # Navigate to the page
-driver.get('https://www.onefc.com/athletes/jonathan-haggerty/')
-name = driver.find_element(By.CLASS_NAME, 'use-letter-spacing-hint')
-wins = driver.find_element(By.CLASS_NAME, 'wins')
-losses = driver.find_element(By.CLASS_NAME, 'losses')
+driver.get('https://www.onefc.com/athletes/martial-art/muay-thai/')
 
-# Navigating class by class
-stats_section = driver.find_element(By.CLASS_NAME, 'simple-stats-list')
-value = stats_section.find_element(By.CLASS_NAME, 'value')
+fighter_cards = driver.find_elements(By.CLASS_NAME, 'simple-post-card')
 
+csv_file = open('fighters.csv', mode='w', newline='', encoding='utf-8')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Name', 'Wins', 'Losses', 'Finishes'])
 
-print(name.text)
-print(wins.text)
-print(losses.text)
-print("Finishes: ",value.text)
+for card in fighter_cards:
+    link = card.find_element(By.TAG_NAME, 'a').get_attribute('href')
+    driver.get(link)
+     # Wait for the page to load
+
+    try:
+        name = driver.find_element(By.CLASS_NAME, 'use-letter-spacing-hint').text
+    except:
+        name = 'N/A'
+    try:
+        wins = driver.find_element(By.CLASS_NAME, 'wins').text
+        wins = wins.split("-")[-1].strip()  # Extract only the number
+    except:
+        wins = 'N/A'
+    try:
+        losses = driver.find_element(By.CLASS_NAME, 'losses').text
+        losses = losses.split("-")[-1].strip()  # Extract only the number
+    except:
+        losses = 'N/A'
+    try:
+        value = driver.find_element(By.CSS_SELECTOR,
+                                    '#site-main > div:nth-child(3) > div > div > div > div:nth-child(3) > div > div > div.simple-stats-list > div:nth-child(1) > div.value').text
+    except:
+        value = 'N/A'
+
+    csv_writer.writerow([name, wins, losses, value])
+
+    driver.back()
+      # Wait for the page to load
+
+csv_file.close()
+driver.quit()
+
+    
 
 
 
